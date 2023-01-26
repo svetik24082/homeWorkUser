1cc0f845291ed9eebf67b94739d25a0f9851a171
Света <svetik24082@mail.ru>
2023-01-26 11:27:22 +0500

    1 коммит

---
+++ src/main/java/com/example/homeworkuser/HomeWorkUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkUserApplication.class, args);
+    }
+
+}
